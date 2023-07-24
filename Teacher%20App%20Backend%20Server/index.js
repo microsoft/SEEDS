315798547db231350ee2139f459a6724a086b60a
Manikanteswar-Punnam <v-punnamm@icrosoft.com>
2023-07-24 11:38:11 +0000

    rate Lmiter added to Teacher App Backend Server

--- Teacher%20App%20Backend%20Server/index.js
+++ Teacher%20App%20Backend%20Server/index.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cors = require('cors') 
 const bodyParser = require("body-parser");
+const rateLimit = require('express-rate-limit');
+
 const morgan = require(path.join(__dirname,"morganConfig.js"))
 const dotenv = require("dotenv/config");
 const verifyToken = require("./auth/verifyToken.js")
-
-
 const callRouter = require("./routes/callRouter.js")
 const teacherRouter = require("./routes/teacherRouter.js")
 const contentRouter = require("./routes/contentRouter")
@@ -18,8 +18,16 @@ const { constants } = require("zlib");
 
 
 const app = express();
-app.use(morgan('dev'))
 
+// Define the rate limiter options
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes - The time window for which requests are counted.
+  max: 100, // 100 requests - The maximum number of requests per IP within the time window.
+});
+
+// Apply the rate limiter middleware to all requests
+app.use(morgan('dev'))
+app.use(limiter);
 app.use(cors())
 app.use("/call", verifyToken, bodyParser.json(), callRouter);
 app.use("/teacher", verifyToken, bodyParser.json(), teacherRouter);
