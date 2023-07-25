fb8a40fd06fd07107635ad14abc2e6f24386e6fd
Manikanteswar-Punnam <v-punnamm@microsoft.com>
2023-07-25 07:39:22 +0000

    added few more conditions to check if the number of contacts limited exceeded or not after receiving request from android app to make conference call

--- IVR%20Server%20Container%20Group/IVR-Server/middleware/conferenceCall.js
+++ IVR%20Server%20Container%20Group/IVR-Server/middleware/conferenceCall.js
@@ -23,35 +23,47 @@ const validateConferenceCallRequest = async (req,res,next) => {
 }
 
 const validateMobileNumbers = (req,res,next) => {
-    const phoneNumbers = req.body.phoneNumbers
-    for(const phoneNumber of phoneNumbers){
-        if(typeof phoneNumber !== "string"){
-            return res.status(400).json({message:"PhoneNumber Type must be string"})
-        }
-        else if(phoneNumber.startsWith("+")){
-            return res.status(400).json({message:"PhoneNumber must not start with +"})
-        }
-        else if(!phoneNumber.startsWith("91")){
-            return res.status(400).json({message:"PhoneNumber must start with 91"})
-        }
-        else if(phoneNumber.length !== 12){
-            return res.status(400).json({message:"PhoneNumber length must be 12 including country code 91"})
-        }
+    const length = req.body.phoneNumbers.length
+    if(length > 100){
+        return res.status(400).json({message:"Number of Contacts Limit Exceeded"})
     }
-    next()
+    else{
+        for(let i=0;i<length;i++){
+            var phoneNumber = req.body.phoneNumbers[i]
+            if(typeof phoneNumber !== "string"){
+                return res.status(400).json({message:"PhoneNumber Type must be string"})
+            }
+            else if(phoneNumber.startsWith("+")){
+                return res.status(400).json({message:"PhoneNumber must not start with +"})
+            }
+            else if(!phoneNumber.startsWith("91")){
+                return res.status(400).json({message:"PhoneNumber must start with 91"})
+            }
+            else if(phoneNumber.length !== 12){
+                return res.status(400).json({message:"PhoneNumber length must be 12 including country code 91"})
+            }
+        }
+        next()
+    }
 }
 
 const validateNames = (req,res,next) => {
-    const names = req.body.names
-    for(const name of names){
-        if(typeof name !== 'string'){
-            return res.status(400).json({message:"Name Type must be string"})
-        }
-        else if(name.trim().length === 0){
-            return res.status(400).json({message:"Name must not be empty"})
-        }
+    const length = req.body.names.length
+    if(length > 100){
+        return res.status(400).json({message:"Number of Contacts Limit Exceeded"})
     }
-    next()
+    else{
+        for(let i=0;i<length;i++){
+            var name = req.body.names[i]
+            if(typeof name !== 'string'){
+                return res.status(400).json({message:"Name Type must be string"})
+            }
+            else if(name.trim().length === 0){
+                return res.status(400).json({message:"Name must not be empty"})
+            }
+        }
+        next()
+    }
 }
 
 const removeDuplicateMobileNumbers = (req,res,next) => {
@@ -59,18 +71,22 @@ const removeDuplicateMobileNumbers = (req,res,next) => {
     const filteredPhoneNumbers = []
     const filteredNames = []
     const length = req.body.phoneNumbers.length
-
-    for(let i=0;i<length;i++){
-        const phoneNumber = req.body.phoneNumbers[i]
-        if(!phoneNumberSet.has(phoneNumber)){
-            phoneNumberSet.add(phoneNumber)
-            filteredPhoneNumbers.push(phoneNumber)
-            filteredNames.push(req.body.names[i])
-        }
+    if(length > 100){
+        return res.status(400).json({message:"Number of Contacts Limit Exceeded"})
     }
-    req.body.phoneNumbers = filteredPhoneNumbers
-    req.body.names = filteredNames
-    next()
+    else{
+        for(let i=0;i<length;i++){
+            const phoneNumber = req.body.phoneNumbers[i]
+            if(!phoneNumberSet.has(phoneNumber)){
+                phoneNumberSet.add(phoneNumber)
+                filteredPhoneNumbers.push(phoneNumber)
+                filteredNames.push(req.body.names[i])
+            }
+        }
+        req.body.phoneNumbers = filteredPhoneNumbers
+        req.body.names = filteredNames
+        next()
+    }
 }
 
 const removeContactsExistInAnotherConference = (req,res,next) => {
