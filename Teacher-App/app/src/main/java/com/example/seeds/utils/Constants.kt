334443ed481b320f5ff8e3faf3b8e98cbbdbfbb6
roshni-poddar <roshni.gryffindor@gmail.com>
2024-04-17 21:35:00 +0530

    [Android App] reverted the internet changes and fixed Base URL

--- Teacher-App/app/src/main/java/com/example/seeds/utils/Constants.kt
+++ Teacher-App/app/src/main/java/com/example/seeds/utils/Constants.kt
@@ -2,8 +2,8 @@ package com.example.seeds.utils
 
 class Constants {
     companion object {
-//        const val BASE_URL = "https://seeds-teacherapp.azurewebsites.net"
-        const val BASE_URL = "https://1f6e-122-172-87-186.ngrok-free.app"
+        const val BASE_URL = "https://seeds-teacherapp.azurewebsites.net"
+//        const val BASE_URL = "https://1f6e-122-172-87-186.ngrok-free.app"
         const val APP_VERSION = "2"
     }
 }
