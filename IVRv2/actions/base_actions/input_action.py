db6dae06442c9c6fe17b0b1525958f18b1b24673
kchourasia <kavyansh@KCHOURASIA-SB3.fareast.corp.microsoft.com>
2024-04-28 01:12:12 +0530

    InputAction to include "eventAPI" instead of complete URL - makes serialized FSM IVR server URL agnostic

--- IVRv2/actions/base_actions/input_action.py
+++ IVRv2/actions/base_actions/input_action.py
@@ -2,13 +2,13 @@ from base_classes.action import Action
 
 class InputAction(Action):
     """You can use the input action to collect digits or speech input by the person you are calling."""
-    def __init__(self, type_: [str], eventUrl: str, **kwargs):
+    def __init__(self, type_: [str], eventApi: str, **kwargs):
         self.type = type_
-        self.eventUrl = eventUrl
+        self.eventApi = eventApi
         self.extra_args = kwargs
         
     def get(self):
         raise NotImplementedError("Get() Function called on Base Action `InputAction`")
     
     def __str__(self):
-        return f"InputAction: {self.type} {self.eventUrl} {self.extra_args}"
+        return f"InputAction: {self.type} {self.eventApi} {self.extra_args}"
